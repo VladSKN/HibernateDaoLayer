b2b788e430579233fdbe0992b317bcb2ac391c7f
Vlad Semikin <vladsemikin3@gmail.com>
2022-03-11 16:57:01 +0200

    dao_layer_with_hibernate

--- src/main/java/ru/netology/hibernatedaolayer/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernatedaolayer/repository/PersonRepository.java
@@ -1,29 +1,22 @@
 package ru.netology.hibernatedaolayer.repository;
 
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 import ru.netology.hibernatedaolayer.entity.Person;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class PersonRepository {
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    public PersonRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
-    }
 
     public List<Person> getPersonsByCity(String city) {
-        SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("city", city);
-        return namedParameterJdbcTemplate.query(
-                "SELECT * FROM person where city_of_living = :city", namedParameters,
-                (resultSet, i) ->
-                        new Person(resultSet.getString("name"), resultSet.getString("surname"),
-                                resultSet.getInt("age"), resultSet.getInt("phone_number"),
-                                resultSet.getString("city_of_living")));
+        return entityManager.createQuery("SELECT person FROM Person person where person.cityOfLiving = :city")
+                .setParameter("city", city)
+                .getResultList();
     }
 }
