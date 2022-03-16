728dd10f6cde01840402334830140964af2cefa8
Vlad Semikin <vladsemikin3@gmail.com>
2022-03-16 15:12:05 +0200

    add @Query

--- src/main/java/ru/netology/hibernatedaolayer/repository/CrudPersonRepository.java
+++ src/main/java/ru/netology/hibernatedaolayer/repository/CrudPersonRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.hibernatedaolayer.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.netology.hibernatedaolayer.entity.Person;
@@ -9,7 +10,13 @@ import java.util.Optional;
 
 @Repository
 public interface CrudPersonRepository extends CrudRepository<Person, Integer> {
+
+    @Query(value = "select p from Person p where p.cityOfLiving = :cityOfLiving")
     List<Person> findByCityOfLiving(String cityOfLiving);
+
+    @Query(value = "select p from Person p where p.age <= :age order by p.age asc")
     List<Person> findByAgeLessThanOrderByAgeAsc(int age);
+
+    @Query(value = "select p from Person p where p.name = :name and p.surname = :surname")
     Optional<Person> findByNameAndSurname(String name, String surname);
 }
